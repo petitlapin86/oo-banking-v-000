ac1b6a642b467d3356084e2472d8eff63dc5e471
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 20:43:59 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,10 +2,27 @@ class BankAccount
 
 attr_accessor :balance, :status
 attr_reader :name
+
 def initialize(name)
   @name = name
   @balance = 1000
   @status = "open"
 end
+
+def deposit
+# can deposit money into its account
+end 
+
+def display_balance
+   # can display its balance
+end 
+
+def valid?
+  #is valid with an open status and a balance greater than 0
+end 
+
+def close_account
+# can close its account  
+end 
 
 end#ends class

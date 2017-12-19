fc5b315a9e824e692eade3d73d98883d808958b6
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 21:12:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,4 +16,36 @@ def valid?
 sender.valid? && receiver.valid? #can check both methods are valid 
 end
 
+def execute_transaction #executes transaction between two accounts 
+  if valid? && sender.balance > amount && self.status == "pending"
+    sender.balance -= amount #subtract balance from sender
+    receiver.balance += amount #add balance to receiver 
+    self.status = "complete" 
+  else
+    reject_transfer #if not valid account reject transfer 
+  end
+end
+
+
+def reverse_transfer
+  if valid? && receiver.balance > amount && self.status == "complete"
+    receiver.balance -= amount
+    sender.balance += amount
+    self.status = "reversed"
+  else
+    reject_transfer
+  end
+end
+
+
+def reject_transfer
+  self.status = "rejected"
+  "Transaction rejected. Please check your account balance."
+end
+
+
+
+
+
+
 end #ends class
